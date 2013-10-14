c6375ff833d733dcad2feae5212e568647694e61
Wolfram <wolfram.soyka@gmail.com>
2013-10-14 13:26:40 +0200

    Chat Activity geadded

---
+++ src/com/activities/Activity_Chat.java
@@ -0,0 +1,16 @@
+package com.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Wolfram
+ * Date: 14.10.13
+ * Time: 13:04
+ */
+public class Activity_Chat extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
