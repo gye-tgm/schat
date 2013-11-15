8a54fe3bc2fe287693fe2a515e675164ff959fce
efrantar-tgm <efrantar@student.tgm.ac.at>
2013-11-15 14:31:45 +0100

    message structure change

---
+++ src/com/data/Content.java
@@ -0,0 +1,35 @@
+package com.data;
+
+import java.io.Serializable;
+
+/**
+ * This is the super-class of all message contents. The message content, is the part of the message, which will be sealed before
+ * transmission.
+ * @author Elias Frantar
+ * @version 15.11.2013
+ */
+public abstract class Content implements Serializable { // there should not exist any instances of this class
+
+    /**
+     * The different content-types of a message.
+     */
+    public enum Type {
+        CHAT_MESSAGE
+    }
+
+    protected Type type; // the type of this content
+
+    /**
+     * Returns the type of this content.
+     * @return the content type (contained in the Type-enum)
+     */
+    public Type getType() {
+        return type;
+    }
+
+    /**
+     * Returns the content, properly formatted and readable in a String.
+     * @return the content in a String
+     */
+    public abstract String toString(); // must be overridden by every sub-class
+}
