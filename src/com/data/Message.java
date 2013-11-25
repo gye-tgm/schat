78ff8cec80b97b571ca353bd26e8c0fc593bcec9
Wolfram <wolfram.soyka@gmail.com>
2013-11-25 13:30:05 +0100

    added getTimestampString()

--- src/com/data/Message.java
+++ src/com/data/Message.java
@@ -6,9 +6,10 @@ import java.util.Date;
 
 /**
  * This class represents a plain message(nothing is encrypted or authenticated). This message can contain any type of "Content".
+ *
+ * @param <C> the type of the Content, which will be stored in this class
  * @author Elias Frantar
  * @version 15.11.2013
- * @param <C> the type of the Content, which will be stored in this class
  */
 public class Message<C extends Content> implements Serializable {
 
@@ -21,10 +22,11 @@ public class Message<C extends Content> implements Serializable {
 
     /**
      * Creates a new message from the given information
+     *
      * @param timestamp the time the message was created
-     * @param sender the sender of the message
-     * @param receiver the receiver of the message
-     * @param content the content of the message
+     * @param sender    the sender of the message
+     * @param receiver  the receiver of the message
+     * @param content   the content of the message
      */
     public Message(Date timestamp, String sender, String receiver, C content) {
         this.timestamp = timestamp;
@@ -35,9 +37,10 @@ public class Message<C extends Content> implements Serializable {
 
     /**
      * Creates a new message without content. This constructor should only be called from the SecureMessage-class.
+     *
      * @param timestamp the time the message is created
-     * @param sender the sender of the message
-     * @param receiver the receiver of the message
+     * @param sender    the sender of the message
+     * @param receiver  the receiver of the message
      */
     protected Message(Date timestamp, String sender, String receiver) { // this constructor should only be accessed from the secure message class, so protected
         this.timestamp = timestamp;
@@ -47,6 +50,7 @@ public class Message<C extends Content> implements Serializable {
 
     /**
      * Returns this message as a properly formatted and readable String.
+     *
      * @return this message as a String
      */
     public String toString() {
@@ -60,6 +64,7 @@ public class Message<C extends Content> implements Serializable {
 
     /**
      * Returns the timestamp, sender and receiver of this message in a properly formatted and readable String.
+     *
      * @return the timestamp, sender and receiver as a String
      */
     protected String requiredInfoToString() { // should be accessible for sub-classes
@@ -73,16 +78,24 @@ public class Message<C extends Content> implements Serializable {
         return result.toString();
     }
 
+    public String getTimestampString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm dd.MM.yyyy");
+        return sdf.format(timestamp);
+    }
+
     /* once data is saved in an object of this class, it should not be modified anymore */
     public Date getTimestamp() {
         return timestamp;
     }
+
     public String getSender() {
         return sender;
     }
+
     public String getReceiver() {
         return receiver;
     }
+
     public C getContent() {
         return content;
     }
