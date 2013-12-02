efd5485ae50a476c54ca2a2a4b549d2892ead14d
gye-tgm <gye@student.tgm.ac.at>
2013-12-02 15:09:19 +0100

    Added JavaDoc

--- common/src/data/SQLiteManager.java
+++ common/src/data/SQLiteManager.java
@@ -1,6 +1,5 @@
 package data;
 
-import crypto.CryptoConstants;
 import crypto.Cryptography;
 
 import javax.crypto.SecretKey;
@@ -10,18 +9,25 @@ import java.security.KeyPair;
 import java.security.PublicKey;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * @author Gary Ye
  * @version 11/30/13
+ *          This class can interact with an SQLite database and it offers
+ *          the needed methods to retrieve or update data.
  */
 public class SQLiteManager {
-    // suggestion: using the jdbc pooling method described here:
-    // http://www.javaranch.com/journal/200601/JDBCConnectionPooling.html
-    private String url;
+    private String url; // the connection url, needed to specify the database to connect to.
 
+    /**
+     * Constructs a SQLite Manager, which can do update or queries
+     * on the given database. The SQLiteManager needs the name of the
+     * database to work on. If the name of the database is "client.db"
+     * then one should pass this string to the constructor.
+     *
+     * @param db the database name
+     */
     public SQLiteManager(String db) {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -32,32 +38,27 @@ public class SQLiteManager {
     }
 
     /**
-     * Calls the create script
+     * Calls the given create script. The create script is only allowed to have DDL statements like
+     * CREATE, DROP, ALTER.
+     *
+     * @param createScriptFile the filename of the create script
      */
     public void createTables(String createScriptFile) {
-        try (
-                Connection connection = DriverManager.getConnection(url);
-                Statement statement = connection.createStatement();
-        ){
-            String sqlScript;
-            try {
-                sqlScript = new Scanner(new File(createScriptFile)).useDelimiter("\\A").next();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                return;
-            }
-
+        try (Connection connection = DriverManager.getConnection(url);
+             Statement statement = connection.createStatement();) {
+            String sqlScript = new Scanner(new File(createScriptFile)).useDelimiter("\\A").next();
             String[] sqlQueries = sqlScript.split(";");
             for (String query : sqlQueries) {
                 statement.executeUpdate(query);
             }
-        } catch (SQLException e) {
+        } catch (SQLException | FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     /**
      * Return the user from the given id
+     *
      * @return null if the user was not found or otherwise the data of the user
      */
     public User getUserFromGivenId(String id) {
@@ -69,7 +70,7 @@ public class SQLiteManager {
                 Statement statement = connection.createStatement();
                 ResultSet rs = statement.executeQuery(query)
         ) {
-            if(!rs.next())
+            if (!rs.next())
                 return null;
             publicKey = Cryptography.getPublicKeyFromBytes(rs.getBytes("public_key"));
             secretKey = Cryptography.getSecretKeyFromBytes(rs.getBytes("symmetric_key")); // sprint(publicKeyBytes);
@@ -81,6 +82,7 @@ public class SQLiteManager {
 
     /**
      * Returns the public of the given user
+     *
      * @param id the given id
      * @return the public key of the given user
      */
@@ -90,14 +92,17 @@ public class SQLiteManager {
 
     /**
      * Returns whether the user exists or not.
+     *
      * @param id the given user id
      * @return whether the user exists or not
      */
-    public boolean userExists(String id){
+    public boolean userExists(String id) {
         return getUserFromGivenId(id) != null;
     }
+
     /**
-     * Execute a simple query
+     * Execute a simple DDL query like INSERT, UPDATE or ALTER.
+     *
      * @param sql the sql query
      */
     public void executeQuery(String sql) {
@@ -119,14 +124,14 @@ public class SQLiteManager {
      */
     public void insertUser(User user) {
         String sql = "INSERT INTO user VALUES(?,?,?) ";
-        try (Connection connection = DriverManager.getConnection(url);
+        try (Connection connection = getConnection();
         ) {
             PreparedStatement pstmt = connection.prepareStatement(sql);
             pstmt.setString(1, user.getId());
             // In SQLite you set blob as bytes
-            if(user.getPublicKey() != null)
+            if (user.getPublicKey() != null)
                 pstmt.setBytes(2, user.getKeyPair().getPublic().getEncoded());
-            if(user.getSecretKey() != null)
+            if (user.getSecretKey() != null)
                 pstmt.setBytes(3, user.getSecretKey().getEncoded());
             pstmt.executeUpdate();
         } catch (SQLException ex) {
@@ -135,10 +140,9 @@ public class SQLiteManager {
     }
 
     /**
-     * Load all users from the database and
-     * return a list of them in the lexicographical order.
+     * Returns a list of all users in the lexicographical order from the database.
      *
-     * @return all users in the lexicographical order
+     * @return all users in the lexicographical order as a list,
      */
     public ArrayList<User> loadUsers() {
         String sql = "SELECT id, public_key, symmetric_key FROM user ORDER BY id ASC";
@@ -161,6 +165,7 @@ public class SQLiteManager {
 
     /**
      * Get a connection from the driver manager
+     *
      * @return a connection
      * @throws SQLException
      */
@@ -170,11 +175,12 @@ public class SQLiteManager {
 
     /**
      * Remove the user with the given id from the database
+     *
      * @param id the id of the user to remove
      * @return if the operation was successful
      */
     public boolean removeUser(String id) {
-        executeQuery(String.format("DELETE FROM user WHERE id = '%s'",  id)); // TODO: check if there is SQL injection?!
+        executeQuery(String.format("DELETE FROM user WHERE id = '%s'", id)); // TODO: check if an SQL injection can occur?!
         return true;
     }
 }
--- common/src/data/contents/PublicKeyResponse.java
+++ common/src/data/contents/PublicKeyResponse.java
@@ -9,29 +9,49 @@ import java.security.PublicKey;
  * @version 12/1/13
  */
 public class PublicKeyResponse extends Content {
-
     private String requestId;
     private PublicKey publicKey;
 
+    /**
+     * Constructs a public key response, which will be sent back
+     * from the server as a response of the PublicKeyRequest
+     * @param requestId the id of the requested user
+     * @param publicKey the public key of the requested user
+     */
     public PublicKeyResponse(String requestId, PublicKey publicKey) {
         this.type = Type.PUBLIC_KEY_RESPONSE;
         this.requestId = requestId;
         this.publicKey = publicKey;
     }
 
+    /**
+     * @return the string
+     */
     @Override
     public String toString() {
         return "Public key of " + requestId + " is inside";
     }
 
+    /**
+     * Returns the request id
+     * @return the request id
+     */
     public String getRequestId() {
         return requestId;
     }
 
+    /**
+     * Return the public key
+     * @return the public key
+     */
     public PublicKey getPublicKey() {
         return publicKey;
     }
 
+    /**
+     * Return if the user exists
+     * @return whether the user exists or not
+     */
     boolean userNotExisting(){
         return publicKey == null;
     }
