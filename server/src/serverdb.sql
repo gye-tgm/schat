e19821aa9eaf89f650ee58be01587184e297700b
gye-tgm <gye@student.tgm.ac.at>
2013-12-02 15:55:08 +0100

    Server create script modified

--- server/src/serverdb.sql
+++ server/src/serverdb.sql
@@ -1,14 +1,13 @@
+DROP TABLE IF EXISTS user;
 CREATE TABLE user (
     id            TEXT,
     public_key    BLOB,
-    symmetric_key BLOB,     /* symmetric key of the client, cannot be read! */
+    symmetric_key BLOB,
     PRIMARY KEY(id)
 );
-
+DROP TABLE IF EXISTS message;
 CREATE TABLE message (
-    sender_id       TEXT,
     receiver_id     TEXT,
-    timestamp       INTEGER, /* unix time format */
-    content         TEXT,
-    PRIMARY KEY(sender_id, receiver_id, timestamp)
+    content         BLOB,
+    PRIMARY KEY(receiver_id, content)
 );
