4a829aab675064c52080ad79aa76bd9b6eaa089c
gye-tgm <gye@student.tgm.ac.at>
2013-12-08 19:13:37 +0100

    Added some resources

---
+++ serverdb.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+    id            TEXT,
+    public_key    BLOB,
+    symmetric_key BLOB,
+    PRIMARY KEY(id)
+);
+DROP TABLE IF EXISTS message;
+CREATE TABLE message (
+    receiver_id     TEXT,
+    timestamp       INT,
+    content         BLOB,
+    PRIMARY KEY(receiver_id, timestamp)
+);
