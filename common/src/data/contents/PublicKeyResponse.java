fe3168f7501869144bcfe9163e0e8dad41f17eed
gye-tgm <gye@student.tgm.ac.at>
2013-12-15 14:03:03 +0100

    Public key response byte[] fix

--- common/src/data/contents/PublicKeyResponse.java
+++ common/src/data/contents/PublicKeyResponse.java
@@ -1,5 +1,6 @@
 package data.contents;
 
+import crypto.Cryptography;
 import data.Content;
 
 import java.security.PublicKey;
@@ -10,18 +11,19 @@ import java.security.PublicKey;
  */
 public class PublicKeyResponse extends Content {
     private String requestId;
-    private PublicKey publicKey;
+    private byte[] publicKey;
 
     /**
      * Constructs a public key response, which will be sent back
      * from the server as a response of the PublicKeyRequest
+     *
      * @param requestId the id of the requested user
      * @param publicKey the public key of the requested user
      */
     public PublicKeyResponse(String requestId, PublicKey publicKey) {
         this.type = Type.PUBLIC_KEY_RESPONSE;
         this.requestId = requestId;
-        this.publicKey = publicKey;
+        this.publicKey = publicKey.getEncoded();
     }
 
     /**
@@ -34,6 +36,7 @@ public class PublicKeyResponse extends Content {
 
     /**
      * Returns the request id
+     *
      * @return the request id
      */
     public String getRequestId() {
@@ -42,17 +45,19 @@ public class PublicKeyResponse extends Content {
 
     /**
      * Return the public key
+     *
      * @return the public key
      */
     public PublicKey getPublicKey() {
-        return publicKey;
+        return Cryptography.getPublicKeyFromBytes(publicKey);
     }
 
     /**
      * Return if the user exists
+     *
      * @return whether the user exists or not
      */
-    boolean userNotExisting(){
+    boolean userNotExisting() {
         return publicKey == null;
     }
 }
